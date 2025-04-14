d3be34eb3e8dba19ee71a24046dcd3ee4eeb8940
alhogan <98360253+alhogan@users.noreply.github.com>
2025-04-14 11:20:19 -0500

    Comment out jobs

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,10 +1,10 @@
-"""Module for design jobs."""
+# """Module for design jobs."""
 
-from nautobot.apps.jobs import register_jobs
-from nautobot_design_builder.util import load_jobs
+# from nautobot.apps.jobs import register_jobs
+# from nautobot_design_builder.util import load_jobs
 
-from .tcp_connectivity_check import ConnectivityCheckTask
-from . command_getter import CommandExecution
+# from .tcp_connectivity_check import ConnectivityCheckTask
+# from . command_getter import CommandExecution
 
-load_jobs("jobs")
-register_jobs(ConnectivityCheckTask, CommandExecution)
+# load_jobs("jobs")
+# register_jobs(ConnectivityCheckTask, CommandExecution)
