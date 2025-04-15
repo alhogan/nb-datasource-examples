a81e4c05f6d87d5a83ce6646a729f747f323c568
alhogan <98360253+alhogan@users.noreply.github.com>
2025-04-15 16:37:18 -0500

    Add missing path

--- designs/jobs.py
+++ designs/jobs.py
@@ -1,5 +1,5 @@
 from .ipam_site_design.jobs import IpamSiteDesign
 from .pop_design.jobs import PopDesign
-from .new_device_registration import NewDeviceRegistrationDesign
+from .new_device_registration.jobs import NewDeviceRegistrationDesign
 
 __all__ = ("IpamSiteDesign", "PopDesign", "NewDeviceRegistrationDesign")
