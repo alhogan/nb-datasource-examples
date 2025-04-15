662ad6f06eed1a9913418ed945b371cdb97c7a83
alhogan <98360253+alhogan@users.noreply.github.com>
2025-04-15 16:33:46 -0500

    Added New device regisration job

--- designs/jobs.py
+++ designs/jobs.py
@@ -1,4 +1,5 @@
 from .ipam_site_design.jobs import IpamSiteDesign
 from .pop_design.jobs import PopDesign
+from .new_device_registration import NewDeviceRegistrationDesign
 
-__all__ = ("IpamSiteDesign", "PopDesign")
+__all__ = ("IpamSiteDesign", "PopDesign", "NewDeviceRegistrationDesign")
