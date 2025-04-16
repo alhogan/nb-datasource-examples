f02472610d2d1d6f6563b3d510027134ba5433c6
alhogan <98360253+alhogan@users.noreply.github.com>
2025-04-15 22:03:13 -0500

    Import new branch jobs

--- designs/jobs.py
+++ designs/jobs.py
@@ -1,5 +1,10 @@
 from .ipam_site_design.jobs import IpamSiteDesign
-from .pop_design.jobs import PopDesign
+from .pop_design.jobs import CreateBase, CreateBranch
 from .new_device_registration.jobs import NewDeviceRegistrationDesign
 
-__all__ = ("IpamSiteDesign", "PopDesign", "NewDeviceRegistrationDesign")
+__all__ = (
+    "IpamSiteDesign",
+    "CreateBase",
+    "CreateBranch",
+    "NewDeviceRegistrationDesign",
+)
